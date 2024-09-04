cc56ebfc272fc69de1cfcaa5b5ef2be580f0e337
Nimesha Abesinghe <abesinghenimesha20@gmail.com>
2024-09-04 18:57:21 +0530

    set tailwind css

---
+++ Frontend/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Welcome to MediMeet</h1>
+        <p>This is a simple React application.</p>
+        <hr />
+      <h2>About Me</h2>
+    </div>
+  )
+}
+
+export default Header
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+  <h1>Welcome to the Counter App</h1>
+    </>
+  )
+}
+
+export default App
